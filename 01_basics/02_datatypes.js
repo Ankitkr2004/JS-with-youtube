00666f03b8c6dcd000c9284446e41fa48547c349
Ankit kumar <143319891+Ankitkr2004@users.noreply.github.com>
2024-09-10 12:52:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) //we are using noje js not browser
+
+console.log(3+3);
+let name="ankit"
+let age=20
+let isLoggedIn = false
+// number => 2 to power 53
+// bigint - use for big number than upper number
+// boolean - true/false
+// string => " "
+// null => standlone value
+// undefined => 
+// symbol - uniqueness for symbol
+// object - 
+
+console.log(typeof "ankit");// string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
