8042dee6ef23082766c6a7de8f323346006d3fae
Ankit kumar <143319891+Ankitkr2004@users.noreply.github.com>
2024-09-11 05:41:22 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score="33abc";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// not a pure number or NaN
+// for null it return 0
+//for undefined it return NaN
+
+let isLoggedIn="ankit";
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; 0=> false
+// "" => false
+// "ankit" => true
+
+let num=33;
+let stringNum=String(num);
+console.log(stringNum);
+console.log(typeof stringNum);
+
+//--------------operations------------------------
+let value=3;
+let neg = -value;
+console.log(neg);
+
+let str1="ankit";
+let str2=" kumar";
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+ 
